97983644cdd5b954126e8c82af2344cf0ca3c343
Quyen Le <quyen.le@eastplayers.io>
2023-02-14 15:17:57 +0700

    minify

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
+import bakedEnv from "rollup-plugin-baked-env";
+import replace from "@rollup/plugin-replace";
+import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 
 export default [
@@ -22,6 +25,31 @@ export default [
       babel({
         exclude: ["node_modules/**"],
       }),
+      terser(),
+    ],
+  },
+  {
+    input: "src/index.ts",
+
+    output: {
+      name: "edgee_tracking",
+      file: "lib/index.min.js",
+      format: "iife",
+      extend: true,
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      }),
+      json(),
+      typescript(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: ["node_modules/**"],
+      }),
+      terser(),
     ],
   },
   // {
