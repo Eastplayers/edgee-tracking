d094db2970f7287a40a64ffc672b1593affedbb8
Quyen Le <quyen.le@eastplayers.io>
2023-02-14 15:23:13 +0700

    bump version

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
-import bakedEnv from "rollup-plugin-baked-env";
 import replace from "@rollup/plugin-replace";
 import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
