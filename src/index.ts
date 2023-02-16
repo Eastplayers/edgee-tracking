de711a969ceccb7bdda56296ded6ec7f9c45947c
Quyen Le <quyen.le@eastplayers.io>
2023-02-16 09:00:36 +0700

    add logEvent function

--- src/index.ts
+++ src/index.ts
@@ -68,8 +68,79 @@ const init = () => {
   }
 };
 
+const logEvent = (name: string, payload: Record<string, any>) => {
+  if (typeof window !== "undefined") {
+    const deviceData = deviceDetect(undefined);
+
+    fetch("https://geolocation-db.com/json/", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then(({ IPv4, ...rest }) => {
+        const data = {
+          source: window.location.href,
+          payload: {
+            browser: browserName,
+            device: {
+              os_name: deviceData.osName,
+              os_version: deviceData.osVersion,
+              user_agent: deviceData.userAgent,
+            },
+            ip: IPv4,
+            geolocation: rest,
+            ad_blocked: false,
+            origin: window.location.origin,
+            event: name,
+            event_data: payload,
+          },
+          timestamp: new Date().toISOString(),
+        };
+        fetch("https://api-cdp-staging.edgee.io/api/v1/events", {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+      })
+      .catch((err) => {
+        const data = {
+          source: window.location.href,
+          payload: {
+            browser: browserName,
+            device: {
+              os_name: deviceData.osName,
+              os_version: deviceData.osVersion,
+              user_agent: deviceData.userAgent,
+            },
+            ip: null,
+            geolocation: null,
+            ad_blocked: true,
+            origin: window.location.origin,
+            event: name,
+            event_data: payload,
+          },
+          timestamp: new Date().toISOString(),
+        };
+        fetch("https://api-cdp-staging.edgee.io/api/v1/events", {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+      });
+  }
+};
+
 const EdgeeTracking = {
   init,
+  logEvent,
 };
 
 export default EdgeeTracking;
