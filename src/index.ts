6da87bc0dcc12c20802a6e6fe184eda255c88289
Quyen Le <quyen.le@eastplayers.io>
2023-02-18 15:27:35 +0700

    add id

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,50 @@
 import { browserName, deviceDetect } from "react-device-detect";
 
-const sendPageViewEvent = () => {
+const URL = "https://api-cdp-staging.edgee.io/api/v1/events";
+const LOCATION_URL = "https://geolocation-db.com/json";
+
+const setCookie = (name: string, value: string) => {
+  var expires = "";
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+};
+
+const getCookie = (name: string) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts?.pop?.()?.split(";").shift();
+};
+
+const sendEvent = (eventName = "page_view", eventPayload?: any) => {
   if (typeof window !== "undefined") {
     const deviceData = deviceDetect(undefined);
+    let id = getCookie("id");
 
-    fetch("https://geolocation-db.com/json/", {
+    if (!id) {
+      id = `${new Date().getTime()}`;
+      setCookie("id", id);
+    }
+
+    const data = {
+      source: "WEB",
+      payload: {
+        id,
+        browser: browserName,
+        device: {
+          os_name: deviceData.osName,
+          os_version: deviceData.osVersion,
+          user_agent: deviceData.userAgent,
+        },
+        ip: null,
+        geolocation: null,
+        ad_blocked: true,
+        origin: window.location.origin,
+        event: eventName,
+        event_data: eventPayload,
+      },
+      timestamp: new Date().toISOString(),
+    };
+
+    fetch(LOCATION_URL, {
       method: "GET",
       headers: {
         accept: "application/json",
@@ -12,51 +52,27 @@ const sendPageViewEvent = () => {
     })
       .then((res) => res.json())
       .then(({ IPv4, ...rest }) => {
-        const data = {
-          source: window.location.href,
+        const newData = {
+          ...data,
           payload: {
-            browser: browserName,
-            device: {
-              os_name: deviceData.osName,
-              os_version: deviceData.osVersion,
-              user_agent: deviceData.userAgent,
-            },
+            ...data.payload,
             ip: IPv4,
             geolocation: rest,
             ad_blocked: false,
-            origin: window.location.origin,
-            event: "page_view",
           },
-          timestamp: new Date().toISOString(),
         };
-        fetch("https://api-cdp-staging.edgee.io/api/v1/events", {
+
+        fetch(URL, {
           method: "POST",
           headers: {
             accept: "application/json",
             "content-type": "application/json",
           },
-          body: JSON.stringify(data),
+          body: JSON.stringify(newData),
         });
       })
       .catch((err) => {
-        const data = {
-          source: window.location.href,
-          payload: {
-            browser: browserName,
-            device: {
-              os_name: deviceData.osName,
-              os_version: deviceData.osVersion,
-              user_agent: deviceData.userAgent,
-            },
-            ip: null,
-            geolocation: null,
-            ad_blocked: true,
-            origin: window.location.origin,
-            event: "page_view",
-          },
-          timestamp: new Date().toISOString(),
-        };
-        fetch("https://api-cdp-staging.edgee.io/api/v1/events", {
+        fetch(URL, {
           method: "POST",
           headers: {
             accept: "application/json",
@@ -70,7 +86,7 @@ const sendPageViewEvent = () => {
 
 const init = () => {
   if (typeof window !== "undefined") {
-    sendPageViewEvent();
+    sendEvent();
 
     ["pushState"].forEach((changeState) => {
       // @ts-ignore
@@ -80,7 +96,7 @@ const init = () => {
         apply(target, thisArg, argList) {
           const [state, title, url] = argList;
           if (window.location.origin !== window.location.origin + url) {
-            sendPageViewEvent();
+            sendEvent();
           }
 
           return target.apply(thisArg, argList);
@@ -91,73 +107,7 @@ const init = () => {
 };
 
 const logEvent = (name: string, payload: Record<string, any>) => {
-  if (typeof window !== "undefined") {
-    const deviceData = deviceDetect(undefined);
-
-    fetch("https://geolocation-db.com/json/", {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then(({ IPv4, ...rest }) => {
-        const data = {
-          source: window.location.href,
-          payload: {
-            browser: browserName,
-            device: {
-              os_name: deviceData.osName,
-              os_version: deviceData.osVersion,
-              user_agent: deviceData.userAgent,
-            },
-            ip: IPv4,
-            geolocation: rest,
-            ad_blocked: false,
-            origin: window.location.origin,
-            event: name,
-            event_data: payload,
-          },
-          timestamp: new Date().toISOString(),
-        };
-        fetch("https://api-cdp-staging.edgee.io/api/v1/events", {
-          method: "POST",
-          headers: {
-            accept: "application/json",
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-      })
-      .catch((err) => {
-        const data = {
-          source: window.location.href,
-          payload: {
-            browser: browserName,
-            device: {
-              os_name: deviceData.osName,
-              os_version: deviceData.osVersion,
-              user_agent: deviceData.userAgent,
-            },
-            ip: null,
-            geolocation: null,
-            ad_blocked: true,
-            origin: window.location.origin,
-            event: name,
-            event_data: payload,
-          },
-          timestamp: new Date().toISOString(),
-        };
-        fetch("https://api-cdp-staging.edgee.io/api/v1/events", {
-          method: "POST",
-          headers: {
-            accept: "application/json",
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-      });
-  }
+  sendEvent(name, payload);
 };
 
 const EdgeeTracking = {
