7e75d7f894caa884533f2bc7f6a90a603a57d92c
Quyen Le <quyen.le@eastplayers.io>
2023-03-02 22:13:32 +0700

    add css link to sample project

--- example/src/pages/_document.tsx
+++ example/src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        href="https://unpkg.com/edgee-tracking@1.0.14/lib/bundle.css"
+        rel="stylesheet"
+        type="text/css"
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
